416670d0b0037f1f30167bd691c010802c851c4e
Samuel E. Moelius III <sam@moeli.us>
2021-04-03 20:21:49 -0400

    Add warnings

--- cargo-dylint/src/main.rs
+++ cargo-dylint/src/main.rs
@@ -1,8 +1,7 @@
-use anyhow::Result;
 use std::env;
 use std::ffi::OsString;
 
-pub fn main() -> Result<()> {
+pub fn main() -> dylint::ColorizedResult<()> {
     env_logger::init();
 
     let args: Vec<_> = env::args().map(OsString::from).collect();
---
+++ dylint/src/error.rs
@@ -0,0 +1,34 @@
+use ansi_term::Color::{Red, Yellow};
+
+pub struct ColorizedError<E>(E)
+where
+    E: std::fmt::Debug;
+
+impl<E> ColorizedError<E>
+where
+    E: std::fmt::Debug,
+{
+    pub fn new(error: E) -> Self {
+        ColorizedError(error)
+    }
+}
+
+// smoelius: The use of `\r` is a bit of a hack, but it works, most notably with `anyhow` backtraces
+// (which require a nightly compiler, BTW). Another way might be to implement the `Termination`
+// trait, but that trait is still unstable: https://github.com/rust-lang/rust/issues/43301
+impl<E> std::fmt::Debug for ColorizedError<E>
+where
+    E: std::fmt::Debug,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "\r{}: {:?}", Red.bold().paint("Error"), self.0)
+    }
+}
+
+pub type ColorizedResult<T> = Result<T, ColorizedError<anyhow::Error>>;
+
+pub fn warn(opts: &crate::Dylint, message: &str) {
+    if !opts.quiet {
+        println!("{}: {}", Yellow.bold().paint("Warning"), message);
+    }
+}
