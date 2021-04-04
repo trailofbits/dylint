c7d2056f72d0cdf466aa87b35523693b068d4b5e
Samuel E. Moelius III <sam@moeli.us>
2021-04-03 20:05:11 -0400

    Add license check

---
+++ scripts/check_licenses.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# set -x
+set -euo pipefail
+
+if [[ $# -ne 0 ]]; then
+    echo "$0: expect no arguments" >&2
+    exit 1
+fi
+
+cargo license |
+while read X; do
+    echo "$X" | grep -w 'Apache\|MIT'
+done
