b3e06005c2bfcae7fcc86e3b605c3b05813859e6
Samuel E. Moelius III <sam@moeli.us>
2021-06-12 18:12:51 -0400

    Add message regarding RUST_BACKTRACE

--- cargo-dylint/src/main.rs
+++ cargo-dylint/src/main.rs
@@ -1,10 +1,19 @@
-use std::env;
+use dylint_internal::env::{self, enabled};
 use std::ffi::OsString;
 
 pub fn main() -> dylint::ColorizedResult<()> {
     env_logger::init();
 
-    let args: Vec<_> = env::args().map(OsString::from).collect();
+    let args: Vec<_> = std::env::args().map(OsString::from).collect();
 
-    dylint::cargo_dylint(&args)
+    let result = dylint::cargo_dylint(&args);
+
+    if result.is_err() && enabled(env::RUST_BACKTRACE) {
+        eprintln!(
+            "If you don't see a backtrace below, it could be because `cargo-dylint` wasn't built \
+            with a nightly compiler."
+        );
+    }
+
+    result
 }
