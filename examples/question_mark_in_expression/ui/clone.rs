b4667c1e9fc944919645698741b4d10dd6d31f9d
Samuel E. Moelius III <sam@moeli.us>
2021-06-13 06:04:26 -0400

    Rustfmt `clone.rs`

--- examples/question_mark_in_expression/ui/clone.rs
+++ examples/question_mark_in_expression/ui/clone.rs
@@ -8,11 +8,15 @@ fn main() {
 struct Error;
 
 impl From<git2::Error> for Error {
-    fn from(_: git2::Error) -> Self { Self }
+    fn from(_: git2::Error) -> Self {
+        Self
+    }
 }
 
 impl From<std::io::Error> for Error {
-    fn from(_: std::io::Error) -> Self { Self }
+    fn from(_: std::io::Error) -> Self {
+        Self
+    }
 }
 
 fn clone() -> Result<(), Error> {
