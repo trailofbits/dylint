7204bce39dc4540601e4548e695d9176e3527ed1
Samuel E. Moelius III <sam@moeli.us>
2021-09-19 20:24:22 -0400

    Share target directories amonng metadata libraries

--- internal/src/rustup.rs
+++ internal/src/rustup.rs
@@ -14,6 +14,22 @@ impl SanitizeEnvironment for crate::Command {
     }
 }
 
+// smoelius: Consider carefully whether you need to call this function! In most cases, the toolchain
+// you want is not the one returned by rustup.
+pub fn active_toolchain(path: &Path) -> Result<String> {
+    let output = Command::new("rustup")
+        .sanitize_environment()
+        .current_dir(path)
+        .args(&["show", "active-toolchain"])
+        .output()?;
+    let stdout = std::str::from_utf8(&output.stdout)?;
+    stdout
+        .splitn(2, ' ')
+        .next()
+        .map(ToOwned::to_owned)
+        .ok_or_else(|| anyhow!("Could not determine active toolchain"))
+}
+
 pub fn toolchain_path(path: &Path) -> Result<PathBuf> {
     let output = Command::new("rustup")
         .sanitize_environment()
