84f1c5ce6990e0568ea20a1a98bfc94a7dcc924b
Samuel E. Moelius III <sam.moelius@trailofbits.com>
2021-11-08 11:51:21 -0500

    Update upgrade_examples.sh

--- scripts/upgrade_examples.sh
+++ scripts/upgrade_examples.sh
@@ -7,6 +7,8 @@ if [[ $# -ne 0 ]]; then
     exit 1
 fi
 
+SCRIPTS="$(dirname "$(realpath "$0")")"
+
 cd "$(dirname "$0")"/..
 
 for EXAMPLE in examples/*; do
@@ -14,10 +16,12 @@ for EXAMPLE in examples/*; do
         continue
     fi
 
-    # smoelius: For now, ignore `allow_clippy`.
+    # smoelius: `allow_clippy` is handled with `clippy` below.
     if [[ "$EXAMPLE" = 'examples/allow_clippy' ]]; then
         continue
     fi
+
+    PREV_TAG="$(sed -n 's/^clippy_utils\>.*\(\<tag = "[^"]*"\).*$/\1/;T;p' "$EXAMPLE"/Cargo.toml)"
 
     cargo run -p cargo-dylint -- dylint --upgrade "$EXAMPLE"
 
@@ -29,6 +33,15 @@ for EXAMPLE in examples/*; do
         sed -i "s/^\\(clippy_lints\>.*\\)\<tag = \"[^\"]*\"\\(.*\\)$/\1$TAG\2/" Cargo.toml
 
         popd
+
+        # smoelius: If `clippy`'s `rust-toolchain` file changed, upgrade `allow_clippy` to the Rust
+        # version that `clippy` used previously. Note that `clippy` can be upgraded without its
+        # `rust-toolchain` file changing.
+        if ! git diff --exit-code "$EXAMPLE"/rust-toolchain; then
+            PREV_VERSION="$(echo "$PREV_TAG" | sed 's/^\<tag = "rust-\([^"]*\)"$/\1/')"
+            cargo run -p cargo-dylint -- dylint --upgrade examples/allow_clippy --rust-version "$PREV_VERSION"
+        fi
     fi
 done
 
+"$SCRIPTS"/update_lockfiles.sh
