8d6e289c24b40129a4a5934e59169b77e1ab15a4
Samuel Moelius <sam.moelius@trailofbits.com>
2022-02-08 16:55:40 +0000

    Expand functions `nonreentrant_function_in_test` checks for

--- examples/nonreentrant_function_in_test/src/nonreentrant_function_in_test.rs
+++ examples/nonreentrant_function_in_test/src/nonreentrant_function_in_test.rs
@@ -57,6 +57,17 @@ const BLACKLIST: &[&[&str]] = &[
     &paths::ENV_REMOVE_VAR,
     &paths::ENV_SET_CURRENT_DIR,
     &paths::ENV_SET_VAR,
+    &paths::FS_COPY,
+    &paths::FS_CREATE_DIR,
+    &paths::FS_CREATE_DIR_ALL,
+    &paths::FS_HARD_LINK,
+    &paths::FS_REMOVE_DIR,
+    &paths::FS_REMOVE_DIR_ALL,
+    &paths::FS_REMOVE_FILE,
+    &paths::FS_RENAME,
+    &paths::FS_SET_PERMISSIONS,
+    &paths::FS_SOFT_LINK,
+    &paths::FS_WRITE,
 ];
 
 impl EarlyLintPass for NonreentrantFunctionInTest {
