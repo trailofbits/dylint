adc418268b64c5550b81232b777758492058e92c
Samuel E. Moelius III <sam@moeli.us>
2022-07-27 20:26:55 -0400

    Make most `internal` modules opt-in
    This reduces driver dependencies, for example.

--- internal/src/lib.rs
+++ internal/src/lib.rs
@@ -1,27 +1,36 @@
+#[cfg(feature = "cargo")]
 pub mod cargo;
 
+#[cfg(feature = "command")]
 mod command;
+#[cfg(feature = "command")]
 pub use command::*;
 
 pub mod env;
 
+#[cfg(feature = "examples")]
 pub mod examples;
 
 mod filename;
 pub use filename::{library_filename, parse_path_filename};
 
-#[cfg(feature = "git2")]
+#[cfg(feature = "git")]
 mod git;
-#[cfg(feature = "git2")]
+#[cfg(feature = "git")]
 pub use git::*;
 
+#[cfg(feature = "packaging")]
 pub mod packaging;
 
 pub mod paths;
 
+#[cfg(feature = "rustup")]
 pub mod rustup;
 
+#[cfg(feature = "sed")]
 mod sed;
+#[cfg(feature = "sed")]
 pub use sed::find_and_replace;
 
+#[cfg(feature = "testing")]
 pub mod testing;
