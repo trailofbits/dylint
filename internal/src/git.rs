4c6262b55d2cec46a6328b998f16570952a2a65a
Samuel E. Moelius III <samuel.moelius@trailofbits.com>
2022-07-31 15:53:19 -0400

    Retry failing `git clone`s

--- internal/src/git.rs
+++ internal/src/git.rs
@@ -3,11 +3,23 @@ use git2::Repository;
 use if_chain::if_chain;
 use std::path::Path;
 
+// smoelius: I think this imitates Cargo's default behavior:
+// https://doc.rust-lang.org/cargo/reference/config.html#netretry
+const N_RETRIES: usize = 2;
+
 // smoelius: I think I may have run into https://github.com/libgit2/libgit2/issues/5294 a few times,
 // but I don't know of a good general-purpose solution. TODO: Investigate whether/how Cargo's
 // wrappers handle this.
 pub fn clone(url: &str, refname: &str, path: &Path) -> Result<Repository> {
-    let repository = Repository::clone(url, path)?;
+    let mut result = Repository::clone(url, path);
+    for _ in 0..N_RETRIES {
+        if result.is_err() {
+            result = Repository::clone(url, path);
+        } else {
+            break;
+        }
+    }
+    let repository = result?;
 
     checkout(&repository, refname)?;
 
