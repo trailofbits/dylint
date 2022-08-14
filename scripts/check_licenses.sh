81331813e8bbe58564aafd4f095e526a116b357f
Samuel Moelius <35515885+smoelius@users.noreply.github.com>
2022-08-14 10:46:30 -0400

    Update check_licenses.sh

--- scripts/check_licenses.sh
+++ scripts/check_licenses.sh
@@ -8,7 +8,8 @@ if [[ $# -ne 0 ]]; then
     exit 1
 fi
 
-cargo license |
+find . -mindepth 2 -name Cargo.toml |
+xargs -n 1 cargo license --manifest-path |
 while read X; do
     # smoelius: Exception for Cargo dependencies.
     if [[ "$X" = 'MPL-2.0+ (3): bitmaps, im-rc, sized-chunks' ]]; then
@@ -16,5 +17,8 @@ while read X; do
     fi
     # smoelius: Good explanation of the differences between the BSD-3-Clause and MIT licenses:
     # https://opensource.stackexchange.com/a/582
-    echo "$X" | grep -w 'Apache\|BSD-3-Clause\|MIT'
+    if ! grep -w 'Apache\|BSD-3-Clause\|ISC\|MIT\|N/A' <(echo "$X") >/dev/null; then
+        echo "$X"
+        exit 1
+    fi
 done
