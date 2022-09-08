218ba522e899ad139b9c53ac3c983905ba5d4c56
Samuel E. Moelius III <samuel.moelius@trailofbits.com>
2022-09-08 12:11:41 -0400

    Add test

--- examples/src/lib.rs
+++ examples/src/lib.rs
@@ -1,6 +1,7 @@
 #[cfg(test)]
 mod test {
     use dylint_internal::{examples::iter, rustup::SanitizeEnvironment};
+    use std::{ffi::OsStr, fs::read_to_string};
 
     #[test]
     fn examples() {
@@ -15,6 +16,24 @@ mod test {
             .current_dir(path)
             .success()
             .unwrap();
+        }
+    }
+
+    #[test]
+    fn examples_have_identical_cargo_configs() {
+        let mut prev = None;
+        for path in iter().unwrap() {
+            let path = path.unwrap();
+            if path.file_name() == Some(OsStr::new("straggler")) {
+                continue;
+            }
+            let config_toml = path.join(".cargo").join("config.toml");
+            let curr = read_to_string(config_toml).unwrap();
+            if let Some(prev) = &prev {
+                assert_eq!(*prev, curr);
+            } else {
+                prev = Some(curr);
+            }
         }
     }
 }
