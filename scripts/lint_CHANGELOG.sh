8e07952123b867794ef306df561a5f88e4ecf390
Samuel Moelius <samuel.moelius@trailofbits.com>
2023-01-11 14:57:55 +0000

    Lint CHANGELOG.md

---
+++ scripts/lint_CHANGELOG.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+
+# shellcheck disable=SC2001
+
+# set -x
+set -euo pipefail
+
+if [[ $# -ne 0 ]]; then
+    echo "$0: expect no arguments" >&2
+    exit 1
+fi
+
+SCRIPTS="$(dirname "$(realpath "$0")")"
+WORKSPACE="$(realpath "$SCRIPTS"/..)"
+
+cd "$WORKSPACE"
+
+grep -o '\[[0-9A-Fa-f]*\]([^)]*)' CHANGELOG.md |
+while read -r LINK; do
+    TEXT="$(echo "$LINK" | sed 's/^\[\([^]]*\)\](.*)$/\1/')"
+    URL="$(echo "$LINK" | sed 's/^\[[^]]*\](\(.*\))$/\1/')"
+    if [[
+        "$TEXT" = "${TEXT,,}" &&
+        ( ${#TEXT} -eq 7 || "$TEXT" = 'c28639ee' ) &&
+        $(expr "$URL" : "https://.*/commit/${TEXT}[0-9a-z]*") -eq ${#URL}
+    ]]; then
+        continue
+    fi
+    echo "bad link: $LINK" >&2
+    exit 1
+done
+
+grep -o '\[#[0-9]*\]([^)]*)' CHANGELOG.md |
+while read -r LINK; do
+    N="$(echo "$LINK" | sed 's/^\[#\([^]]*\)\](.*)$/\1/')"
+    URL="$(echo "$LINK" | sed 's/^\[[^]]*\](\(.*\))$/\1/')"
+    if [[
+        $(expr "$URL" : "https://.*/issues/$N") -eq ${#URL} ||
+        $(expr "$URL" : "https://.*/pull/$N") -eq ${#URL}
+    ]]; then
+        continue
+    fi
+    echo "bad link: $LINK" >&2
+    exit 1
+done
