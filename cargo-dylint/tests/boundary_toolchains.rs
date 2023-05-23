3dfcb0f5b547712e235ea92e309bb84bab84e55c
Samuel Moelius <samuel.moelius@trailofbits.com>
2023-05-23 01:17:31 +0000

    Address rust-lang/rust#111633

--- cargo-dylint/tests/boundary_toolchains.rs
+++ cargo-dylint/tests/boundary_toolchains.rs
@@ -7,9 +7,19 @@ use test_log::test;
 
 // smoelius: The channel date is one day later than the `rustc --version` date.
 // smoelius: Put recent boundaries first, since they're more likely to cause problems.
-const BOUNDARIES: [(&str, &str); 3] = [
+// smoelius: The relevant PRs and merge commits appear before each boundary.
+const BOUNDARIES: [(&str, &str); 4] = [
+    // https://github.com/rust-lang/rust/pull/111633
+    // https://github.com/rust-lang/rust/commit/08efb9d652c840715d15954592426e2befe13b36
+    ("2023-05-18", "2023-05-19"),
+    // https://github.com/rust-lang/rust/pull/106810
+    // https://github.com/rust-lang/rust/commit/65d2f2a5f9c323c88d1068e8e90d0b47a20d491c
     ("2023-01-19", "2023-01-20"),
+    // https://github.com/rust-lang/rust/pull/101501
+    // https://github.com/rust-lang/rust/commit/87788097b776f8e3662f76627944230684b671bd
     ("2022-09-08", "2022-09-09"),
+    // https://github.com/rust-lang/rust/pull/98975
+    // https://github.com/rust-lang/rust/commit/0ed9c64c3e63acac9bd77abce62501696c390450
     ("2022-07-14", "2022-07-15"),
 ];
 
