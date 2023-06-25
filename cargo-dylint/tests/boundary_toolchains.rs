968c375b0eb07cc9cc93b835b106138d93e8e1db
Samuel Moelius <samuel.moelius@trailofbits.com>
2023-06-25 22:40:05 +0000

    Update `boundary_toolchains` test

--- cargo-dylint/tests/boundary_toolchains.rs
+++ cargo-dylint/tests/boundary_toolchains.rs
@@ -7,7 +7,7 @@ use tempfile::tempdir;
 // smoelius: The channel date is one day later than the `rustc --version` date.
 // smoelius: Put recent boundaries first, since they're more likely to cause problems.
 // smoelius: The relevant PRs and merge commits appear before each boundary.
-const BOUNDARIES: [(&str, &str); 5] = [
+const BOUNDARIES: &[(&str, &str)] = &[
     // https://github.com/rust-lang/rust/pull/111748
     // https://github.com/rust-lang/rust/commit/70e04bd88d85cab8ed110ace5a278fab106d0ef5
     ("2023-05-29", "2023-05-30"),
@@ -20,9 +20,11 @@ const BOUNDARIES: [(&str, &str); 5] = [
     // https://github.com/rust-lang/rust/pull/101501
     // https://github.com/rust-lang/rust/commit/87788097b776f8e3662f76627944230684b671bd
     ("2022-09-08", "2022-09-09"),
+    // smoelius: `git2-0.17.2` requires `std::ffi::c_uint`, which was introduced in Rust 1.64.0
+    // (2022-09-22): https://doc.rust-lang.org/stable/std/ffi/type.c_uint.html
     // https://github.com/rust-lang/rust/pull/98975
     // https://github.com/rust-lang/rust/commit/0ed9c64c3e63acac9bd77abce62501696c390450
-    ("2022-07-14", "2022-07-15"),
+    // ("2022-07-14", "2022-07-15"),
 ];
 
 #[test]
