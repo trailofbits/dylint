c14c4784f1dbf0ade59d3c3179432de80d4ed7b0
Samuel Moelius <sam@moeli.us>
2023-07-02 05:14:10 -0400

    Fix adjacent code

--- cargo-dylint/tests/warn.rs
+++ cargo-dylint/tests/warn.rs
@@ -40,10 +40,6 @@ fn no_libraries_were_found() {
 
 #[test]
 fn nothing_to_do() {
-    // smoelius: The code that handles workspace metadata builds the example libraries, so
-    // `examples::build()` is no longer needed here.
-    // dylint_internal::examples::build().unwrap();
-
     std::process::Command::cargo_bin("cargo-dylint")
         .unwrap()
         .args(["dylint"])
--- examples/testing/clippy/tests/ui.rs
+++ examples/testing/clippy/tests/ui.rs
@@ -26,8 +26,6 @@ fn ui() {
     isolate(tempdir.path()).unwrap();
 
     let src_base = tempdir.path().join("tests/ui");
-    // smoelius: I can't remember why disabling `rustfix` was necessary.
-    // disable_rustfix(&src_base).unwrap();
     adjust_macro_use_imports_test(&src_base).unwrap();
 
     // smoelius: `DYLINT_LIBRARY_PATH` must be set before `dylint_libs` is called.
