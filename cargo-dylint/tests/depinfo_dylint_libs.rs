c35a44fa0c624e57724ccfa058e1e4da57bbf270
Samuel Moelius <sam@moeli.us>
2023-10-02 05:47:40 -0400

    Fix #856

---
+++ cargo-dylint/tests/depinfo_dylint_libs.rs
@@ -0,0 +1,31 @@
+use assert_cmd::prelude::*;
+use dylint_internal::env;
+use predicates::prelude::*;
+use std::{env::set_var, process::Command};
+
+#[ctor::ctor]
+fn initialize() {
+    set_var(env::CARGO_TERM_COLOR, "never");
+}
+
+#[test]
+fn depinfo_dylint_libs() {
+    Command::cargo_bin("cargo-dylint")
+        .unwrap()
+        .current_dir("fixtures/depinfo_dylint_libs")
+        .args(["dylint", "--lib", "question_mark_in_expression"])
+        .assert()
+        .stderr(predicate::str::contains(
+            "\nwarning: using the `?` operator within an expression\n",
+        ));
+
+    Command::cargo_bin("cargo-dylint")
+        .unwrap()
+        .current_dir("fixtures/depinfo_dylint_libs")
+        .args(["dylint", "--lib", "try_io_result"])
+        .assert()
+        .stderr(predicate::str::contains(
+            "\nwarning: returning a `std::io::Result` could discard relevant context (e.g., files \
+             or paths involved)\n",
+        ));
+}
