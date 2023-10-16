90f109fe4d903f7dafb1557f3f0d1d7121acca2c
Samuel Moelius <samuel.moelius@trailofbits.com>
2023-10-16 00:00:23 +0000

    Register `incorrect_matches_operation` in general library

--- examples/general/src/lib.rs
+++ examples/general/src/lib.rs
@@ -13,6 +13,7 @@ pub fn register_lints(sess: &rustc_session::Session, lint_store: &mut rustc_lint
     await_holding_span_guard::register_lints(sess, lint_store);
     crate_wide_allow::register_lints(sess, lint_store);
     env_cargo_path::register_lints(sess, lint_store);
+    incorrect_matches_operation::register_lints(sess, lint_store);
     non_local_effect_before_error_return::register_lints(sess, lint_store);
     non_thread_safe_call_in_test::register_lints(sess, lint_store);
     wrong_serialize_struct_arg::register_lints(sess, lint_store);
