trailofbits/dylint

commit 54a50cc670d40b2e6f1c8e56d97562e50ee8219c

Pass `--lib` to `cargo test`

examples/src/lib.rs
@@ -13,12 +13,15 @@ mod test {
         for path in iter(false).unwrap() {
             let path = path.unwrap();
             let file_name = path.file_name().unwrap();
+            // smoelius: Pass `--lib` to `cargo test` to avoid the potential filename collision
+            // associated with building the examples.
             dylint_internal::cargo::test(
                 &format!("example `{}`", file_name.to_string_lossy()),
                 false,
             )
             .sanitize_environment()
             .current_dir(path)
+            .args(["--lib"])
             .success()
             .unwrap();
         }

commit eceeeaac79f7e8c8e19bab7013210161f58c4379

Run example integration tests
Fix a bug introduced by 54a50cc670d40b2e6f1c8e56d97562e50ee8219c

examples/src/lib.rs
@@ -13,13 +13,13 @@ mod test {
         for path in iter(false).unwrap() {
             let path = path.unwrap();
             let file_name = path.file_name().unwrap();
-            // smoelius: Pass `--lib` to `cargo test` to avoid the potential filename collision
-            // associated with building the examples.
+            // smoelius: Pass `--lib --tests` to `cargo test` to avoid the potential filename
+            // collision associated with building the examples.
             dylint_internal::cargo::test(&format!("example `{}`", file_name.to_string_lossy()))
                 .build()
                 .sanitize_environment()
                 .current_dir(path)
-                .args(["--lib"])
+                .args(["--lib", "--tests"])
                 .success()
                 .unwrap();
         }