ebec29ae6456b07f3e235d8f7c007ebaaab4443f
Samuel Moelius <samuel.moelius@trailofbits.com>
2023-12-18 09:15:29 -0500

    Fix `boundary_toolchains` test

--- cargo-dylint/tests/boundary_toolchains.rs
+++ cargo-dylint/tests/boundary_toolchains.rs
@@ -16,19 +16,24 @@ const BOUNDARIES: &[(&str, &str)] = &[
     // https://github.com/rust-lang/rust/pull/111748
     // https://github.com/rust-lang/rust/commit/70e04bd88d85cab8ed110ace5a278fab106d0ef5
     ("2023-05-29", "2023-05-30"),
+    // smoelius: `cargo-util v0.2.7` requires rustc 1.72.0 or newer.
     // https://github.com/rust-lang/rust/pull/111633
     // https://github.com/rust-lang/rust/commit/08efb9d652c840715d15954592426e2befe13b36
-    ("2023-05-18", "2023-05-19"),
+    // ("2023-05-18", "2023-05-19"),
+
+    // smoelius: `home v0.5.9` requires rustc 1.70.0 or newer
     // https://github.com/rust-lang/rust/pull/106810
     // https://github.com/rust-lang/rust/commit/65d2f2a5f9c323c88d1068e8e90d0b47a20d491c
-    // smoelius: `home v0.5.9` requires rustc 1.70.0 or newer
     // ("2023-01-19", "2023-01-20"),
+
     // smoelius: `toml v0.7.8` requires rustc 1.66.0 or newer.
     // https://github.com/rust-lang/rust/pull/101501
     // https://github.com/rust-lang/rust/commit/87788097b776f8e3662f76627944230684b671bd
     // ("2022-09-08", "2022-09-09"),
+
     // smoelius: `git2-0.17.2` requires `std::ffi::c_uint`, which was introduced in Rust 1.64.0
     // (2022-09-22): https://doc.rust-lang.org/stable/std/ffi/type.c_uint.html
+
     // https://github.com/rust-lang/rust/pull/98975
     // https://github.com/rust-lang/rust/commit/0ed9c64c3e63acac9bd77abce62501696c390450
     // ("2022-07-14", "2022-07-15"),
