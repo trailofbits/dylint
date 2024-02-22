535162ee3cd5413e636050cfd65267bae10310f3
Samuel Moelius <samuel.moelius@trailofbits.com>
2024-02-22 00:06:08 +0000

    Eliminate reliance on `sedregex`

--- internal/src/sed.rs
+++ internal/src/sed.rs
@@ -1,17 +1,17 @@
-use anyhow::{anyhow, Context, Result};
+use anyhow::{Context, Result};
+use regex::Regex;
 use std::{
     fs::{read_to_string, write},
     path::Path,
 };
 
-pub fn find_and_replace<I>(path: &Path, commands: I) -> Result<()>
+pub fn find_and_replace<R>(path: &Path, re: &str, replacement: R) -> Result<()>
 where
-    I: IntoIterator,
-    I::Item: AsRef<str>,
+    R: AsRef<str>,
 {
     let before = read_to_string(path)
         .with_context(|| format!("`read_to_string` failed for `{}`", path.to_string_lossy()))?;
-    let after =
-        sedregex::find_and_replace(&before, commands).map_err(|error| anyhow!("{}", error))?;
+    let re = Regex::new(re)?;
+    let after = re.replace_all(&before, replacement.as_ref());
     write(path, after.as_bytes()).map_err(Into::into)
 }
