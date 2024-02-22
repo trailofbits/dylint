2470e695f85cd184ec81d5e6a9d1cc926d619fa8
Samuel Moelius <samuel.moelius@trailofbits.com>
2024-02-22 00:00:38 +0000

    Add `alpine` test

---
+++ tests/alpine/tests/alpine.rs
@@ -0,0 +1,34 @@
+#![cfg(unix)]
+
+use std::{
+    io::{stderr, Write},
+    process::Command,
+};
+
+#[test]
+fn alpine() {
+    let status = Command::new("which").arg("docker").status().unwrap();
+    if !status.success() {
+        #[allow(clippy::explicit_write)]
+        writeln!(
+            stderr(),
+            "Skipping `alpine` test as `docker` could not be found",
+        )
+        .unwrap();
+        return;
+    }
+
+    // smoelius: Don't use `assert_cmd::Command` here because it would hide the output.
+    let status = Command::new("docker")
+        .args([
+            "build",
+            "--progress=plain",
+            "-f",
+            "tests/alpine/Dockerfile",
+            ".",
+        ])
+        .current_dir("../..")
+        .status()
+        .unwrap();
+    assert!(status.success());
+}
