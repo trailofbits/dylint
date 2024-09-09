6022de11dd0a025896d65b6fef2a7eb5f9a2d8db
Samuel Moelius <samuel.moelius@trailofbits.com>
2024-09-09 14:42:27 +0000

    Simplify `escaping_doc_link`

--- examples/supplementary/escaping_doc_link/src/lib.rs
+++ examples/supplementary/escaping_doc_link/src/lib.rs
@@ -4,16 +4,14 @@
 extern crate rustc_ast;
 extern crate rustc_resolve;
 
-use cargo_metadata::{Metadata, MetadataCommand};
 use clippy_utils::diagnostics::span_lint;
-use once_cell::sync::OnceCell;
 use pulldown_cmark::{Options, Parser};
 use rustc_ast::Attribute;
 use rustc_lint::{LateContext, LateLintPass, LintContext};
 use rustc_resolve::rustdoc::{add_doc_fragment, attrs_to_doc_fragments, DocFragment};
-use std::path::{Path, PathBuf};
+use std::path::{absolute, Path, PathBuf};
 
-dylint_linting::impl_late_lint! {
+dylint_linting::declare_late_lint! {
     /// ### What it does
     /// Checks for doc comment links that refer to files outside of their source file's package.
     ///
@@ -32,13 +30,7 @@ dylint_linting::impl_late_lint! {
     /// [docs.rs]: https://docs.rs
     pub ESCAPING_DOC_LINK,
     Warn,
-    "doc comment links that escape their packages",
-    EscapingDocLink::default()
-}
-
-#[derive(Default)]
-struct EscapingDocLink {
-    metadata: OnceCell<Metadata>,
+    "doc comment links that escape their packages"
 }
 
 impl<'tcx> LateLintPass<'tcx> for EscapingDocLink {
@@ -47,27 +39,15 @@ impl<'tcx> LateLintPass<'tcx> for EscapingDocLink {
             return;
         };
 
-        let Some(source_dir) = source_path
-            .local_path()
-            .and_then(Path::parent)
-            .map(|parent| {
-                if parent.as_os_str().is_empty() {
-                    Path::new(".")
-                } else {
-                    parent
-                }
-            })
-        else {
+        let Some(manifest_dir) = source_path.local_path().and_then(|path| {
+            absolute(path)
+                .ok()
+                .map(|absolute_path| strip_suffix(&absolute_path, path))
+        }) else {
             return;
         };
 
-        let metadata = self.metadata(source_dir);
-
-        let source_dir = absolutize(metadata.workspace_root.as_std_path(), source_dir, false);
-
-        let Some(manifest_dir) = find_package(metadata, &source_dir) else {
-            return;
-        };
+        assert!(manifest_dir.is_absolute());
 
         let (fragments, _) = attrs_to_doc_fragments(std::iter::once((attr, None)), true);
 
@@ -89,10 +69,10 @@ impl<'tcx> LateLintPass<'tcx> for EscapingDocLink {
 
             let path = Path::new(path);
 
-            let path = absolutize(manifest_dir, path, true);
+            let path = absolutize(&manifest_dir, path, true);
 
             if path.exists() {
-                if !path.starts_with(manifest_dir) {
+                if !path.starts_with(&manifest_dir) {
                     span_lint(
                         cx,
                         ESCAPING_DOC_LINK,
@@ -107,16 +87,10 @@ impl<'tcx> LateLintPass<'tcx> for EscapingDocLink {
     }
 }
 
-impl EscapingDocLink {
-    fn metadata(&self, source_dir: &Path) -> &Metadata {
-        self.metadata.get_or_init(|| {
-            MetadataCommand::new()
-                .current_dir(source_dir)
-                .no_deps()
-                .exec()
-                .unwrap()
-        })
-    }
+fn strip_suffix(path: &Path, suffix: &Path) -> PathBuf {
+    let n = path.components().count();
+    let m = suffix.components().count();
+    path.components().take(n - m).collect()
 }
 
 // smoelius: `assemble_doc_fragments` is based on code from Clippy's `doc-markdown` lint:
@@ -128,28 +102,6 @@ fn assemble_doc_fragments(fragments: impl IntoIterator<Item = DocFragment>) -> S
     }
     doc.pop();
     doc
-}
-
-fn find_package<'a>(metadata: &'a Metadata, source_dir: &Path) -> Option<&'a Path> {
-    metadata.packages.iter().fold(None, |closest_dir, package| {
-        if let Some(manifest_dir) = package.manifest_path.parent() {
-            if source_dir.starts_with(manifest_dir) {
-                match closest_dir {
-                    None => Some(manifest_dir.as_std_path()),
-                    Some(closest_dir)
-                        if closest_dir.components().count() < manifest_dir.components().count() =>
-                    {
-                        Some(manifest_dir.as_std_path())
-                    }
-                    _ => closest_dir,
-                }
-            } else {
-                closest_dir
-            }
-        } else {
-            closest_dir
-        }
-    })
 }
 
 fn absolutize(base: &Path, path: &Path, normalize: bool) -> PathBuf {
