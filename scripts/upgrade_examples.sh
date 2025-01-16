a5cd0af17bd67181c5180ef3a4cbba83e34e1053
Samuel Moelius <samuel.moelius@trailofbits.com>
2025-01-16 13:40:12 +0000

    Run `cargo-dylint` with `RUST_LOG=debug` in upgrade_examples.sh

--- scripts/upgrade_examples.sh
+++ scripts/upgrade_examples.sh
@@ -29,7 +29,7 @@ for EXAMPLE in examples/general examples/supplementary examples/restriction $EXP
         PREV_REV="$(sed -n 's/^clippy_utils\>.*\(\<\(rev\|tag\) = "[^"]*"\).*$/\1/;T;p' "$EXAMPLE"/Cargo.toml)"
         PREV_CHANNEL="$(sed -n 's/^channel = "[^"]*"$/&/;T;p' "$EXAMPLE"/rust-toolchain)"
 
-        $CARGO_DYLINT upgrade "$EXAMPLE" --auto-correct || true
+        RUST_LOG=debug $CARGO_DYLINT upgrade "$EXAMPLE" --auto-correct
 
         REV="$(sed -n 's/^clippy_utils\>.*\(\<\(rev\|tag\) = "[^"]*"\).*$/\1/;T;p' "$EXAMPLE"/Cargo.toml)"
         sed -i "s/^\(clippy_config\>.*\)\<\(rev\|tag\) = \"[^\"]*\"\(.*\)$/\1$REV\3/" "$EXAMPLE"/Cargo.toml
@@ -53,7 +53,7 @@ for EXAMPLE in examples/general examples/supplementary examples/restriction $EXP
         mv "$EXAMPLE"/Cargo.toml~ "$EXAMPLE"/Cargo.toml
     fi
 
-    $CARGO_DYLINT upgrade "$EXAMPLE" --auto-correct
+    RUST_LOG=debug $CARGO_DYLINT upgrade "$EXAMPLE" --auto-correct
 
     if [[ "$EXAMPLE" = 'internal/template' ]]; then
         mv "$EXAMPLE"/Cargo.toml "$EXAMPLE"/Cargo.toml~
