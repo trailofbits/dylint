4ce065a0d69b2fbdaaf97a7f41062463dceea732
Augustin Villetard <augustin.dev@tutamail.com>
2025-02-13 07:04:56 +0900

    feat(examples)#1225: non existent path in comment

---
+++ examples/supplementary/nonexistent_path_in_comment/ui/main.rs
@@ -0,0 +1,11 @@
+// This path does not exist
+// See ../nonexistent/path/file.rs
+
+// This path should exist
+// See ../src/lib.rs
+
+/* This is a block comment with a nonexistent path
+   See ../another/nonexistent/path/file.go
+*/
+
+fn main() {}
