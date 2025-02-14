70b3de6888d5a030e656e265741e13730d922271
Augustin Villetard <augustin.dev@tutamail.com>
2025-02-14 19:48:06 +0900

    feat: span captured file path, single dot path, update tests

--- examples/supplementary/nonexistent_path_in_comment/ui/main.rs
+++ examples/supplementary/nonexistent_path_in_comment/ui/main.rs
@@ -8,4 +8,10 @@
    See ../another/nonexistent/path/file.go
 */
 
+// Single dot path that does exist
+// ./main.rs
+
+// Single dot path that does not exist
+// The span ./ido/not/exist.rs only points to the path
+
 fn main() {}
