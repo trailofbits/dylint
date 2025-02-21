7cfe12c4ce13593a35feb545fadf79a68c78623c
Augustin Villetard <augustin.dev@tutamail.com>
2025-02-21 18:33:47 +0900

    feat: verify  and  registered lints

--- cargo-dylint/tests/ci.rs
+++ cargo-dylint/tests/ci.rs
@@ -8,6 +8,7 @@ use regex::Regex;
 use semver::{Op, Version};
 use similar_asserts::SimpleDiff;
 use std::{
+    collections::BTreeSet,
     env::{set_current_dir, set_var, var},
     ffi::OsStr,
     fmt::Write as _,
