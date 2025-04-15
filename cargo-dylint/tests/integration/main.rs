22436857d8a764827cc78734988686d455f13b64
Samuel Moelius <samuel.moelius@trailofbits.com>
2025-04-15 22:12:17 +0000

    Fix adjacent code

--- cargo-dylint/tests/integration/main.rs
+++ cargo-dylint/tests/integration/main.rs
@@ -1,3 +1,6 @@
+#![cfg_attr(dylint_lib = "general", allow(crate_wide_allow))]
+#![cfg_attr(dylint_lib = "supplementary", allow(nonexistent_path_in_comment))]
+
 mod depinfo_dylint_libs;
 mod dylint_driver_path;
 mod fix;
