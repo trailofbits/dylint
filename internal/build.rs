d7ac534fb546d49180a69bb502872de7885449ec
Samuel Moelius <samuel.moelius@trailofbits.com>
2025-09-10 14:48:32 +0000

    Shot in the dark to try to resolve link errors on Windows

--- internal/build.rs
+++ internal/build.rs
@@ -7,6 +7,11 @@ fn main() {
 
     #[cfg(feature = "packaging")]
     build_template_tar();
+
+    // smoelius: This fix exists in `git2`'s master branch, but we are using version 0.18. See:
+    // https://github.com/rust-lang/git2-rs/pull/1143
+    #[cfg(all(feature = "git", target_os = "windows"))]
+    println!("cargo:rustc-link-lib=advapi32");
 }
 
 fn is_nightly() -> bool {
