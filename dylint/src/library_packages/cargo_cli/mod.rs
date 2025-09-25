431706e53b83d8bb03e7164f8ff6e8ee9498a6e6
Samuel Moelius <samuel.moelius@trailofbits.com>
2025-09-25 22:23:40 +0000

    Make linters happy

--- dylint/src/library_packages/cargo_cli/mod.rs
+++ dylint/src/library_packages/cargo_cli/mod.rs
@@ -19,7 +19,7 @@ use crate::opts;
 use anyhow::{Context, Result, anyhow, bail, ensure};
 use cargo_metadata::{Metadata, MetadataCommand};
 use cargo_util_schemas::manifest::TomlDetailedDependency;
-use dylint_internal::{CommandExt, cargo::stable_cargo_path, home::cargo_home, packaging::isolate};
+use dylint_internal::{CommandExt, home::cargo_home, packaging::isolate};
 use semver::Version;
 use serde::Serialize;
 use std::{
@@ -290,7 +290,7 @@ fn cargo_fetch(path: &Path) -> Result<std::process::Output> {
 
 fn cargo_metadata(path: &Path) -> Result<Metadata> {
     MetadataCommand::new()
-        .cargo_path(stable_cargo_path())
+        .cargo_path(dylint_internal::cargo::stable_cargo_path())
         .current_dir(path)
         .exec()
         .map_err(Into::into)
@@ -367,8 +367,7 @@ fn find_accessed_subdir<'a>(
 
     ensure!(
         accessed.len() <= 1,
-        "Multiple subdirectories were accessed: {:#?}",
-        accessed
+        "Multiple subdirectories were accessed: {accessed:#?}"
     );
 
     accessed
