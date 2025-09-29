47fe4cf2bded3d05ebe725a9a6977205881d5635
Samuel Moelius <samuel.moelius@trailofbits.com>
2025-09-29 06:47:50 -0400

    Use `args_os()` rather than `args().map(OsString::from)`

--- dylint/src/driver_builder.rs
+++ dylint/src/driver_builder.rs
@@ -61,7 +61,7 @@ use std::ffi::OsString;
 pub fn main() -> Result<()> {
     env_logger::init();
 
-    let args: Vec<_> = env::args().map(OsString::from).collect();
+    let args: Vec<_> = env::args_os().collect();
 
     dylint_driver::dylint_driver(&args)
 }
