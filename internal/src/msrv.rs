c88962eb98b764eef0dc9ba9c06886f8c4f5a7dd
Brannon Dorsey <brannon@brannondorsey.com>
2025-12-10 12:44:53 -0500

    Revert "Try bumping MSRV_CLIPPY_UTILS_REV to a Git OID from April 30th 2025"
    This reverts commit 52da796ba9057bbd406fc0c04d77e4b5404c36a3.

--- internal/src/msrv.rs
+++ internal/src/msrv.rs
@@ -24,7 +24,7 @@ pub const MSRV: &str = "1.88.0";
 pub const MSRV_CHANNEL: &str = "nightly-2025-04-23";
 
 /// The `clippy_utils` Git OID (commit hash) for MSRV.
-pub const MSRV_CLIPPY_UTILS_REV: &str = "dd5113daab0758bdd150bdbf6f16d6f2f86c52cd";
+pub const MSRV_CLIPPY_UTILS_REV: &str = "36c3617bb13b3608170a617eb173e4df96ad3e52";
 
 /// The minimum supported Rust version plus one minor version.
 pub const MSRV_PLUS_1: &str = "1.89.0";
