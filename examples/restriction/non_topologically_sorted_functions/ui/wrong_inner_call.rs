521721382bb8fb6b27e2b0a0f0c470e3123d5684
J4CKVVH173 <i78901234567890@gmail.com>
2025-12-11 14:40:23 +0300

    added comment

--- examples/restriction/non_topologically_sorted_functions/ui/wrong_inner_call.rs
+++ examples/restriction/non_topologically_sorted_functions/ui/wrong_inner_call.rs
@@ -6,6 +6,8 @@ fn baz() {}
 
 fn foo() {
     bar();
+    // This nesting is necessary as part of the logic check.
+    // It is checked that nesting will not affect the rule.
     {
         baz();
     }
