cc34194ebb8ad10b0b999d520f9e554d4ba6245e
Samuel Moelius <samuel.moelius@trailofbits.com>
2026-01-04 11:10:41 -0500

    Bump MSRV and update MSRV comments

--- internal/src/msrv.rs
+++ internal/src/msrv.rs
@@ -18,19 +18,25 @@
 // smoelius: `home@0.5.11` (2024-12-16) requires rustc 1.81.
 // smoelius: `icu_collections@2.0.0` and several other packages require rustc 1.82.
 // smoelius: Edition 2024 was stabilized with Rust 1.85.
-pub const MSRV: &str = "1.88.0";
+// smoelius: `zmij v1.0.1` requires Rust 1.88. However, `zmij`'s build script considers
+// nightly-2025-04-22 Rust 1.88. Hence, the MSRV must be bumped to 1.89.
+pub const MSRV: &str = "1.89.0";
 
-/// The nightly channel date corresponding to MSRV.
-pub const MSRV_CHANNEL: &str = "nightly-2025-04-22";
+/// The channel Clippy was using when it switched to 0.[`MSRV`].
+///
+/// Note that this is not the channel when Rust switched to [`MSRV`].
+pub const MSRV_CHANNEL: &str = "nightly-2025-05-14";
 
-/// The `clippy_utils` Git OID (commit hash) for MSRV.
-pub const MSRV_CLIPPY_UTILS_REV: &str = "ff428d91c2b690b8dbd8cc1e48274870c24fe1e2";
+/// The `clippy_utils` Git OID (commit hash) for [`MSRV`].
+pub const MSRV_CLIPPY_UTILS_REV: &str = "93bd4d893122417b9265563c037f11a158a8e37c";
 
 /// The minimum supported Rust version plus one minor version.
-pub const MSRV_PLUS_1: &str = "1.89.0";
+pub const MSRV_PLUS_1: &str = "1.90.0";
 
-/// The nightly channel date corresponding to MSRV+1.
-pub const MSRV_PLUS_1_CHANNEL: &str = "nightly-2025-05-14";
+/// The channel Clippy was using when it switched to 0.[`MSRV_PLUS_1`].
+///
+/// Note that this is not the channel when Rust switched to [`MSRV_PLUS_1`].
+pub const MSRV_PLUS_1_CHANNEL: &str = "nightly-2025-07-10";
 
-/// The `clippy_utils` Git OID (commit hash) for MSRV+1.
-pub const MSRV_PLUS_1_CLIPPY_UTILS_REV: &str = "93bd4d893122417b9265563c037f11a158a8e37c";
+/// The `clippy_utils` Git OID (commit hash) for [`MSRV_PLUS_1`].
+pub const MSRV_PLUS_1_CLIPPY_UTILS_REV: &str = "4e614bf683fb265079f79268408cd69e361efdcc";
