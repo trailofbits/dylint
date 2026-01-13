30657a9820a8f2c0adb11e0cc8b1c15f05f8db7d
Samuel Moelius <35515885+smoelius@users.noreply.github.com>
2026-01-13 14:11:46 -0500

    Update error.rs

--- dylint/src/error.rs
+++ dylint/src/error.rs
@@ -53,6 +53,6 @@ pub fn warn(opts: &crate::opts::Dylint, message: &str) {
         // smoelius: Writing directly to `stderr` prevents capture by `libtest`.
         #[allow(clippy::panic)]
         writeln!(std::io::stderr(), "{style}Warning{style:#}: {message}")
-            .unwrap_or_else(|error| panic!("Could not write to stderr: {error})"));
+            .unwrap_or_else(|error| panic!("Could not write to stderr: {error}"));
     }
 }
