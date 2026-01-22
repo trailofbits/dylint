e1647fbf7e62f2d857bf7b55669c5792e2573eb8
Samuel Moelius <samuel.moelius@trailofbits.com>
2026-01-22 18:54:36 -0500

    Finish up

--- examples/restriction/const_path_join/ui/const_expr.rs
+++ examples/restriction/const_path_join/ui/const_expr.rs
@@ -13,3 +13,15 @@ fn main() {
     // Test with camino paths
     let _ = camino::Utf8PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("src");
 }
+
+#[allow(dead_code)]
+mod negative_tests {
+    const CATEGORY: &str = "restriction";
+    const LIB_NAME: &str = "const_path_join";
+
+    fn foo() {
+        let _ = std::path::Path::new("examples")
+            .join(CATEGORY)
+            .join(LIB_NAME);
+    }
+}
