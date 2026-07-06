b3f3cc0b49c109052790b2dd6e57103b21534db1
Samuel Moelius <samuel.moelius@trailofbits.com>
2026-07-06 08:01:22 -0400

    Conditionally allow `unused_features` in driver's build script

--- driver/build/main.rs
+++ driver/build/main.rs
@@ -1,3 +1,7 @@
+// smoelius: `proc_macro_hygiene` appears to be unnecessary since:
+// https://github.com/rust-lang/rust/pull/157857
+#![cfg_attr(dylint_lib = "general", allow(crate_wide_allow))]
+#![allow(unused_features)]
 #![feature(proc_macro_hygiene)]
 
 // smoelius: rust-lang/rust-clippy#14705 was merged on 2025-05-05 and Clippy's toolchain was
