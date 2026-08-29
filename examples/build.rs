6ce18532af24da1516e1fa65436c5aea9e15d63d
Samuel Moelius <samuel.moelius@trailofbits.com>
2026-08-29 06:36:06 -0400

    Restrict what Dylint can cause to be built

--- examples/build.rs
+++ examples/build.rs
@@ -1,3 +1,10 @@
+use std::env::var_os;
+
 fn main() {
-    nested_workspace::build().unwrap();
+    // smoelius: Don't allow Dylint itself to cause the examples to be built. Currently, Nested
+    // Workspace does not clear `RUSTC_WORKSPACE_WRAPPER`. Building the `straggler` library with
+    // this environment variable set can cause "incompatible version of rustc" errors.
+    if var_os(dylint_internal::env::RUSTC_WORKSPACE_WRAPPER).is_none() {
+        nested_workspace::build().unwrap();
+    }
 }
